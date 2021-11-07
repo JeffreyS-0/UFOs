fa2c0e9e80d61a0c140a289f640c6a4a4296a6e9
Jeffrey Sykora <jeffsykora1@gmail.com>
2021-11-07 12:44:15 -0600

    fixed issue for loading table

--- static/js/app.js
+++ static/js/app.js
@@ -7,7 +7,7 @@ var tbody = d3.select("tbody");
 // First, clear out any existing data in the table.
 function buildTable(data) {
     tbody.html("");
-}
+
 
 // Next, loop through each object in the data and append a row and cells 
 // for each value in the row
@@ -20,8 +20,10 @@ data.forEach((dataRow) => {
     Object.values(dataRow).forEach((val) => {
         let cell = row.append("td");
         cell.text(val);
-    });
+        }
+    );
 });
+}
 
 function handleClick() {
     // Grab the datetime value from the filter
@@ -34,13 +36,13 @@ function handleClick() {
         //Apply 'filter' to the table data to only keep the
         // rows where the 'datetime' value matches the filter value
         filteredData = filteredData.filter(row => row.datetime === date);
-    };
+    }
 
     // Rebuild the table using the filtered data
     // @NOTE: If no date was entered, then filteredData will
     // just be the original tableData.
     buildTable(filteredData);
-};
+}
 
 // Attach an event to listen for the form button
 d3.selectAll("filter-btn").on("click", handleClick);
